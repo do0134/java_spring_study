6ea14f8df8f6df40f97d11c76cfb03a49a7361b4
do013 <do0134@naver.com>
2024-03-07 23:53:50 +0900

    study: hashmap 코드 까보기

--- src/main/java/com/example/jpatest/JpaTestApplication.java
+++ src/main/java/com/example/jpatest/JpaTestApplication.java
@@ -3,11 +3,18 @@ package com.example.jpatest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 @SpringBootApplication
 public class JpaTestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JpaTestApplication.class, args);
+
+        Map<Object, Object> map = new HashMap<>();
+        Map<Object, Object> map1 = new ConcurrentHashMap<>();
     }
 
 }
