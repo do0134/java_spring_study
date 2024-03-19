2396c8cb7d595e08ba79e901527cbceeb2fdbba4
do013 <do0134@naver.com>
2024-03-19 15:22:30 +0900

    study: map

---
+++ src/main/java/com/example/jpatest/service/MapService.java
@@ -0,0 +1,67 @@
+package com.example.jpatest.service;
+
+import java.util.*;
+
+public class MapService implements Map{
+
+    Map<Object, Object> map = new HashMap<>(32,0.80f);
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return false;
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return false;
+    }
+
+    @Override
+    public Object get(Object key) {
+        return null;
+    }
+
+    @Override
+    public Object put(Object key, Object value) {
+        return null;
+    }
+
+    @Override
+    public Object remove(Object key) {
+        return null;
+    }
+
+    @Override
+    public void putAll(Map m) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Set keySet() {
+        return null;
+    }
+
+    @Override
+    public Collection values() {
+        return null;
+    }
+
+    @Override
+    public Set<Entry> entrySet() {
+        return null;
+    }
+}
